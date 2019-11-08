cb6f3ed4399787b2e23c643fd7b9e86f5448c443
Milena Wojciechowska <wojciechowska.milena@mail.com>
2019-11-08 01:19:25 +0100

    corecting map adding leaflet script

--- dist/js/index.js
+++ dist/js/index.js
@@ -7,4 +7,4 @@ marker.bindPopup("<b>Green Touch</b><br>Address Lorem ipsum.").openPopup();
  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);;
+});
